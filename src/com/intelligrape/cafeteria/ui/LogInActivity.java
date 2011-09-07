fb3c702654f9723f13e1d1c0d35e07f881c6f01f
Imran Ali <imran.ali@intelligrape.com>
2011-09-07 17:09:48 +0530

    first commit

---
+++ src/com/intelligrape/cafeteria/ui/LogInActivity.java
@@ -0,0 +1,13 @@
+package com.intelligrape.cafeteria.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
